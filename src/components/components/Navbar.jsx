ec94a9c053f014e97f78f4b0119fc3d73c097c0e
Kévin Fischer <kevinfischer@live.fr>
2024-01-31 18:24:09 +0100

    :sparkles: added condition to add disconnect button and user name in signInItem if user is logged in

--- src/components/components/Navbar.jsx
+++ src/components/components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';
 import SignInItem from './SignInItem';
 
 function Navbar() {
+  const [userIsLoggedIn] = useState(false);
+  const [userName] = useState('');  
+
   return (
     <nav className='main-nav'>
       <Logo />
-      <SignInItem />
+      <SignInItem isLoggedIn={userIsLoggedIn} userName={userName} />
     </nav>
   );
 }
 
 export default Navbar;
+
--- src/components/components/SignInItem.jsx
+++ src/components/components/SignInItem.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function SignInItem() {
+function SignInItem({ isLoggedIn, userName }) {
+  if (isLoggedIn) {
+    return (
+      <div>
+        <Link to="/user" className="main-nav-item">
+          <i className="fa fa-user-circle"></i>
+          {userName}
+        </Link>
+        <Link to="/" className="main-nav-item">
+          <i className="fa fa-sign-out"></i>
+          Sign Out
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Link to="/signin" className="main-nav-item">
@@ -11,5 +27,10 @@ function SignInItem() {
     </div>
   );
 }
+
+SignInItem.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  userName: PropTypes.string
+};
 
 export default SignInItem;
