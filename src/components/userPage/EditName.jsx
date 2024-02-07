134ab6ba389bfc0a55886dd6378408b69715b3eb
Kévin Fischer <kevinfischer@live.fr>
2024-02-07 12:20:09 +0100

    :lipstick: added style to EditMenu

--- src/components/userPage/EditName.jsx
+++ src/components/userPage/EditName.jsx
@@ -6,21 +6,25 @@ function EditName({ firstName, lastName, onSave, onCancel }) {
   const [newLastName, setNewLastName] = useState(lastName);
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <input
-        type="text"
-        placeholder="First Name"
-        value={newFirstName}
-        onChange={(e) => setNewFirstName(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        value={newLastName}
-        onChange={(e) => setNewLastName(e.target.value)}
-      />
-      <button onClick={() => onSave(newFirstName, newLastName)}>Save</button>
-      <button onClick={onCancel}>Cancel</button>
+    <div className='editMenu'>
+      <div className='editData'>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={newFirstName}
+          onChange={(e) => setNewFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={newLastName}
+          onChange={(e) => setNewLastName(e.target.value)}
+        />
+      </div>
+      <div className='editActions'>
+        <button onClick={() => onSave(newFirstName, newLastName)}>Save</button>
+        <button onClick={onCancel}>Cancel</button>
+      </div>
     </div>
   );
 }
